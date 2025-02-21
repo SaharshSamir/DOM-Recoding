dc95edc5da8c0ec81121b9cbf6735ee68cc9fd1e
ThinkingRocks <saharshsamir10@gmail.com>
2025-02-21 09:44:20 +0530

    added snapshot dir to gitignore

--- record.js
+++ record.js
@@ -13,17 +13,21 @@
   xhr.send(originalDomStatePayload);
 
   const observer = new MutationObserver((mutationList) => {
+    const dmp = new diff_match_patch();
+    //get diff
+    const diff = dmp.diff_main(originalText, revisedText);
+
+    //cleanup diff
+    if (diff.length > 2) {
+      dmp.diff_cleanupSemantic(diff);
+    }
+
+    //get patches
+    const patch_list = dmp.patch_make(originalText, revisedText, diff);
+
+    //apply patch
+    const patched = dmp.patch_apply(patch_list, originalText);
+    console.log(patched);
 
   })
-  // const dmp = new diff_match_patch();
-  // const diff = dmp.diff_main(originalText, revisedText);
-  //
-  // if (diff.length > 2) {
-  //   dmp.diff_cleanupSemantic(diff);
-  // }
-  //
-  // const patch_list = dmp.patch_make(originalText, revisedText, diff);
-  //
-  // const patched = dmp.patch_apply(patch_list, originalText);
-  // console.log(patched);
 })().then(() => console.log('ran script'));
